a37fa344c43535748630c2240c04d05031e291ff
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2022-08-15 13:53:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App API Star Wars',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
